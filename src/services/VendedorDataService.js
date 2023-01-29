e5d654eb02f64be7d473d756a67423df67c96e6f
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-01-28 23:00:25 -0300

    Implementando modo atualizar

--- src/services/VendedorDataService.js
+++ src/services/VendedorDataService.js
@@ -8,6 +8,10 @@ class VendedorDataService {
     cadastrar(vendedor) {
         return http.post('/vendedor', vendedor);
     }
+
+    atualizar(id, vendedor) {
+        return http.put(`/vendedor/${id}`, vendedor);
+    }
 }
 
 export default new VendedorDataService();
