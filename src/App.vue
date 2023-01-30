85fc1786e007842cc981d2a5a707b13691f02163
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-01-30 09:44:40 -0300

    ...

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,21 @@
 
 <template>
 
-<div class="navbar">
+  <div class="navbar">
 
-  <div class="dropdown">
-    <button class="dropbtn">Vendedor 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <router-link to="/vendedor/listar">Listar</router-link>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
+    <div class="dropdown">
+      <button class="dropbtn">Vendedor
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <router-link to="/vendedor/listar">Listar</router-link>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
     </div>
-  </div> 
-</div>
+  </div>
 
-<router-view />
+  <router-view />
 
 </template>
 
